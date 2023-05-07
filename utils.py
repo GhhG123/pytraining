efc726c6e29790c15fea63b06b4401cabce62e68
GhhG123 <100225935+GhhG123@users.noreply.github.com>
2023-05-07 19:24:59 +0800

    Success to getting target link

--- utils.py
+++ utils.py
@@ -5,21 +5,23 @@ from bs4 import BeautifulSoup as BS
 class Scientist:
     def __init__(self, name):
         self.name = name
-        self.dblp_url = 'https://dblp.org/search?q={name}&h=1000&f=0&c=1000'
-
+        self.dblp_url = 'https://dblp.org/search?q='+name+'&h=1000&f=0&c=1000'
+        print(self.dblp_url)
     # get the scientist's articles list
     def get_articlelist(self):
         response = requests.get(self.dblp_url)
         soup = BS(response.text, 'html.parser')
         articlelist = []
         #for article in soup.select
-        li_tags = soup.find_all('li')
-        for li in li_tags:
-            span = li.find('span', class_='title')
-            if span:
-                title = span.text
-                print(title)
-            a = li.find('a')
-            if a:
-                link = a['href']
-                print(link)
+
+        result_list = soup.find("ul", class_="result-list")
+        if result_list:
+            for li in result_list.find_all("li"):
+                a_tag = li.find("a")
+                if a_tag:
+                    href = a_tag.get("href")
+                    span_tag = a_tag.find("span", itemprop="name")
+                    if span_tag:
+                        name = span_tag.text
+                        print(href, name)
+        
